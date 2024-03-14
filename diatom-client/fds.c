b2da2f4c6afe0065843b6d042dc74dee225dafd5
R-Rothrock <=>
2024-03-14 16:54:10 -0500

    Worked with SQLite component

--- diatom-client/fds.c
+++ diatom-client/fds.c
@@ -1,11 +1,14 @@
-// fd.c
+// fds.c
 // handling file descriptiors
 
 // okay, so this is going to have to have a sort of database system
 // preferably in memory.
 // How?
 // We shall simply ("simply?" heh, no) use SQLite to create a
-// "transient in-memory database."
+// "transient in-memory database." In this database we will store the
+// data of the file descriptors.
+
+#include<sqlite.h>
 
 enum fd_type {
   FD_TYPE_FILE,
@@ -18,8 +21,55 @@ struct fd {
   char *realloc;
 }
 
-struct fd *
-getfd(int fd) {
+static sqlite *DB;
+
+static char SQL_INIT_CMD[128] = "\
+CREATE TABLE fds (\
+id      INTEGER PRIMARY KEY AUTOINCREMEMT NOT NULL,\
+type    TEXT NOT NULL,\
+loc     TEXT NOT NULL,\
+realloc TEXT NOT NULL,\
+);"
+
+static char SQL_INSERT_CMD[64] = "\
+INSERT INTO fds (id, type, loc, realloc)\
+VALUES (%s, %s, %s, %s);"
+
+static char SQL_FETCH_CMD[64] = "\
+SELECT *\
+FROM fds\
+WHERE id = %s;"
+
+static char SQL_NEXTFD_CMD[32] = "\
+SELECT MAX(id) FROM fds;
+
+
+
+int fds_init(void)
+{
+  /* run this before all other functions here */
+  
+  int ret;
+  
+  ret = sqlite3_open(":memory:", DB);
+  
+  if(ret != 0)
+  {
+    return ret;
+  }
+
+  ret = sqlite3_exec(&SQL_INIT_CMD, &DB);
+
+  // TODO
+}
+
+int fds_exit(void) {
+  /* run this after all other functions here */
+
+  return sqlite3_close(DB);
+}
+
+struct fd *getfd(int fd) {
   // TODO
 }
 
